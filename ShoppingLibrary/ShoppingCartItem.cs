2028fb66ac97554b7340ee528dbda6e507f3b25e
Ashley <4270837@myuwc.ac.za>
2022-04-28 15:42:19 +0200

    Added classes and code

---
+++ ShoppingLibrary/ShoppingCartItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCartItem 
+    {
+
+
+        private int _productId;
+        private Product _productName = null;
+        
+        
+        
+        public int Quantity { get; set; }
+
+
+
+        public int ProductId
+        {
+         get{return _productId;}
+         set{ _productName = null; 
+                _productId = value;}
+        }
+
+        public Product Prod
+
+        {
+            get
+            {
+            if(_productName == null)
+                {
+                    _productName = new Product(ProductId);
+                }
+                return _productName;
+            }
+            
+        }
+        
+        public decimal itemPrice
+        {
+         get{ return Product.id; }
+        }
+
+        public decimal TotalPrice
+        {
+         get{ return itemPrice * Quantity; }
+        }
+
+        public ShoppingCartItem (int productId)
+        {
+            this.ProductId = productId;
+        }
+
+        public bool Equals(ShoppingCartItem item)
+        {
+            return item.ProductId == this.ProductId;
+        }
+    }
+
+}
